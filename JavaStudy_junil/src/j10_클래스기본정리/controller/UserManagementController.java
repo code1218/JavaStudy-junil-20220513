6ec200c9ba1fbdf5106b16e4c946b1f29ab64b2d
gomaci <91512927+gomaci@users.noreply.github.com>
2022-05-26 21:53:46 +0900

    배열 기초

--- JavaStudy_junil/src/j10_클래스기본정리/controller/UserManagementController.java
+++ JavaStudy_junil/src/j10_클래스기본정리/controller/UserManagementController.java
@@ -6,16 +6,34 @@ import j10_클래스기본정리.service.UserService;
 public class UserManagementController {
 	
 	private UserService userService;
+	private User[] users;
 	
 	public UserManagementController(UserService userService) {
 		this.userService = userService;
+		users = new User[2];
 	}
 
 	public void createUser() {
-		User user = userService.insertUser();
+		int indexResult = findEmptyInUsers();
+		if(indexResult != -1) {
+			users[indexResult] = userService.insertUser();
+			
+			System.out.println("[새로 추가된 사용자]");
+			System.out.println(users[indexResult].toString());
+		}else {
+			System.out.println("더 이상 사용자를 추가할 수 없습니다.");
+		}
 		
-		System.out.println("[새로 추가된 사용자]");
-		System.out.println(user.toString());
+	}
+	
+	private int findEmptyInUsers() {
+		for(int i = 0; i < users.length; i++) {
+			if(users[i] == null) {
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 	
 	public void updateUser() {
@@ -23,3 +41,6 @@ public class UserManagementController {
 	}
 	
 }
+
+
+
